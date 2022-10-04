f20a47dc98db732f7e226a5e050409d55100b9fa
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-10-04 14:09:07 -0300

    :bulb: Add ChangePassword component

--- src/components/User/ChangePassword/index.js
+++ src/components/User/ChangePassword/index.js
@@ -8,54 +8,65 @@ import { BASEURL } from "../../../App";
 
 // Animated Routes
 import {motion} from 'framer-motion';
+import VerifyOTP from './verify';
 
 
 function ChangePassword () {
     const navigate = useNavigate();
 
-    // Fields
-    const [code, setCode] = useState('')
-    const [validCode, setValidCode] = useState(false)
+    // Stage controller
+    const [stage, setStage] = useState(1)
 
-    // Error treatment
+    // Fields
+    const [password1, setPassword1] = useState('')
+    const [password2, setPassword2] = useState('')
+
+    // Fields Validations
+    const [eightCharacters, setEightCharacters] = useState(false)
+    const [oneLetter, setOneLetter] = useState(false)
+    const [oneNumber, setOneNumber] = useState(false)
+    const [passwordsMatch, setPasswordsMatch] = useState(false)
+    useEffect(() => {
+        if (password1.length < 8) setEightCharacters(false); else setEightCharacters(true);
+        if (!password1.match(/[a-zA-Z]/)) setOneLetter(false); else setOneLetter(true);
+        if (!password1.match(/\d/)) setOneNumber(false); else setOneNumber(true);
+    }, [password1])
+    useEffect(() => {
+        if(password1 === password2) setPasswordsMatch(true); else setPasswordsMatch(false)
+    }, [password1, password2])
+
+    // Form Validations
+    const [validForm, setValidForm] = useState(false)
+    useEffect(() => {
+        if (eightCharacters && oneLetter && oneNumber && passwordsMatch)
+            setValidForm(true)
+        else
+            setValidForm(false)
+    }, [eightCharacters, oneLetter, oneNumber, passwordsMatch])
+
+    // API error treatment
     const [error, setError] = useState(false)
     const [errorMsg, setErrorMsg] = useState('')
-    const [success, setSuccess] = useState(false)
-    const [successMsg, setSuccessMsg] = useState('')
     
-
-    useEffect(() => {
-        if(code.length > 3)
-            setValidCode(true)
-            
-        setSuccess(false)
-        setError(false)
-    }, [code])
 
     // Perform redeem
     async function handleSubmit(e){
         e.preventDefault();
-        const url = BASEURL + '/user/redeem'
-        const response = await axios.post(url, {code}, {withCredentials: true}).catch((error) => {
-            setError(true)
-            setValidCode(false)
-            if (error.response.status === 401)
-                setErrorMsg(error.response.data.msg)
-            else if (error.response.status === 400)
-                setErrorMsg('Please, insert a valid code.')
-            else if (error.response.status === 403)
+        setValidForm(false)
+        const url = BASEURL + '/auth/password/reset'
+        const response = await axios.get(
+            url,
+            {withCredentials: true}).catch((error) => {
+                setError(true)
+            if (error.response.status === 403)
                 navigate('/login')
             else
                 setErrorMsg('Server is currently unavailable')
         }).then((response) => {return response})
-        if (response) {
-            setError(false)
-            setValidCode(false)
-            setSuccess(true)
-            setSuccessMsg(response.data.value + ' Pan Coins redeemed succesfully!')
-        }
+        if (response)
+            setStage(2)
+        setValidForm(true)
     }
-
 
     return (
         <motion.main
@@ -66,40 +77,57 @@ function ChangePassword () {
            <div className='container'>
                 <div className='row justify-content-center text-center'>
                     <div className='col-5 mt-3 mb-5 color_third'>
+                        
                         <Link to='/user' className='color_third'>
                             <small className='text-right color_third'>
                                 <strong><u> &#8592; Return to user area</u></strong>
                             </small><br/>
                         </Link>
+
                         <hr className='mt-5'></hr>
+
+                        {(stage === 1)?
+                        <div>
                         <div className='px-5 mt-5'>
                             {error ? <div class="alert alert-danger" role="alert"><strong>Ops!</strong>  {errorMsg}</div>: ''}
-                            {success ? <div class="alert alert-success" role="alert">
-                                        <strong>Yay!</strong>  {successMsg}
-                                     </div>: ''}
                                 
                             <h3><strong>Change your password!</strong></h3>
                             <p>
-                                Before continue, please insert the <strong>verification code</strong>.<br/>
-                                we've just send to your email. It's for your safety.
+                                Your new password must:<br/>
+                                [{eightCharacters ? '✔️': '❌'}]  contain at least <strong>8 characters</strong>.<br/>
+                                [{oneLetter ? '✔️': '❌'}] contain at least <strong>one letter</strong>.<br/>
+                                [{oneNumber ? '✔️': '❌'}] contain at least <strong>one number</strong>.<br/>
+                                [{passwordsMatch ? '✔️': '❌'}] <strong>Be the same in the two fields</strong>.<br/>
                             </p>
                             <input
                             type='text'
                             className='py-1 text-center px-3 w-100'
-                            placeholder='Verification Code'
-                            onChange={(e) => setCode(e.target.value)}/>
-                            <button className='mt-3 btn hvr-wobble-top btn-lg btn-custom_1 w-100 px-4 me-3'>
-                                Verify
+                            placeholder='Enter your new password'
+                            onChange={(e) => setPassword1(e.target.value)}/>
+
+                            <input
+                            type='text'
+                            className='mt-2 py-1 text-center px-3 w-100'
+                            placeholder='Enter your new password again'
+                            onChange={(e) => setPassword2(e.target.value)}/>
+                            <button 
+                            onClick={(e) => handleSubmit(e)}
+                            disabled={!validForm}
+                            className='mb-3 mt-3 btn hvr-wobble-top btn-lg btn-custom_1 w-100 px-4 me-3'>
+                                Continue
                             </button>
-                            <small>
-                            <strong>Didn't receive it?</strong> Click <u><strong>here</strong></u>, we will send it again!<br/>
-                                Be sure to check your <strong>spam</strong> and <strong>trash</strong> folders.
-                            </small>
-
-                            <hr className='mt-5'></hr>
-
                             
-                        </div>      
+                            
+                        </div>
+                    </div>
+                        :
+                        <div className='px-5 mt-5'>
+                            <VerifyOTP data={{
+                                'password1': password1,
+                                'password2': password2
+                            }}/>
+                        </div>}
+                        
                     </div>
                 </div>
             </div>
