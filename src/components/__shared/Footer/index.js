248470959e76db9ba033391eaa6f556871f8e1fc
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-20 12:07:14 -0300

    :arrow_up: Update footer

--- src/components/__shared/Footer/index.js
+++ src/components/__shared/Footer/index.js
@@ -2,7 +2,11 @@ import { useEffect } from "react";
 
 /* Custom Components */
 import SocialIcons from "./SocialIcons";
-import PreFooter from "./PreFooter";
+
+/* Assets */
+import panyartFurryGif from "./assets/img/panyart_furry.gif";
+import heart from "../../../assets/img/heart.png";
+import panCoins from "../../../assets/img/pancoins.png";
 
 import "./assets/styles.css";
 
@@ -12,30 +16,90 @@ function Footer() {
   }, []);
 
   return (
-    <footer className="container-fluid m-0 p-0">
-      <PreFooter />
-      <div className="row justify-content-center text-center">
-        <div className="col-12 col-md-4 patrick-hand color_third">
-          <h4>
-            Interested in my work?
+    <footer className="container-fluid mt-5 p-0">
+      <div className="overlay"></div>
+      <div className="container pt-5">
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <img src={panyartFurryGif} className="img-fluid panyart_gif" />
             <br />
-            <strong>Contact me</strong> through my socials:
-          </h4>
+            <small>
+              Art by{" "}
+              <a
+                target="_blank"
+                className="color_secondary"
+                href="https://twitter.com/Gosetsuki"
+              >
+                <u>
+                  <strong>@gosetsuki</strong>
+                </u>
+              </a>
+            </small>
+            <br />
+            <small>
+              Animated by{" "}
+              <a
+                target="_blank"
+                className="color_secondary"
+                href="https://twitter.com/LightGemArt"
+              >
+                <u>
+                  <strong>@lightgem</strong>
+                </u>
+              </a>
+            </small>
+          </div>
+          <div className="d-none d-md-flex col-md-6 justify-content-center align-items-center">
+            <div className="text-center color_third">
+              <img
+                src={panCoins}
+                className="img-fluid mb-3 px-2 pancoin"
+                width="100px"
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <SocialIcons />
-      <div className="row pt-2">
-        <div className="col-12 text-center bg-white" id="credits">
-          <small>
-            © PanYart | All rights reserved | Developed with ♡ by:{" "}
-            <a
-              className="text-dark"
-              href="https://github.com/nova-script"
-              target="_blank"
-            >
-              <u>@nova-script</u>
-            </a>
-          </small>
+        <div className="row pt-2 mt-3">
+          <div className="col-md-6 text-center color_secondary">
+            <div className="mt-5">
+              <h3>
+                <strong className="stroked-title">
+                  Thanks for coming!
+                  <img className="ms-3" src={heart} width="50px" />
+                </strong>
+              </h3>
+              <p class="color_third">
+                I'm a full-time furry artist working on refsheets and
+                ilustrations!
+                <br />
+                Wanna taste some fresh pieces from the <strong>art oven</strong>
+                ?
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 text-center text-md-start introduction">
+            <p className="color_third text-center mt-5">
+              Interested in my artwork?
+              <br />
+              <strong>Contact me</strong> through my socials:
+              <SocialIcons />
+            </p>
+          </div>
+        </div>
+
+        <div className="row pt-5">
+          <div className="col-12 text-center" id="credits">
+            <small>
+              © PanYart | All rights reserved | Developed with ♡ by:{" "}
+              <a
+                className="text-dark"
+                href="https://github.com/nova-script"
+                target="_blank"
+              >
+                <u>@nova-script</u>
+              </a>
+            </small>
+          </div>
         </div>
       </div>
     </footer>
